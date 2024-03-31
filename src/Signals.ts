c1691c898bff2a42bb8b1a2dc52eb2b17be38d30
Art <arda1murtezan2@gmail.com>
2024-03-31 17:48:22 +0000

    Update Signals.ts

--- src/Signals.ts
+++ src/Signals.ts
@@ -5,7 +5,7 @@ const activeWindow = signal(-1);
 const wallpaper = signal(localStorage.getItem("wallpaper") || "1.webp");
 const pepsimode = signal(false);
 const isAppFullscreen = signal(false);
-const language = signal(localStorage.getItem("language") ? localStorage.getItem("language") : window.location.hostname == 'maximlucas.dev' ? 'fr' : 'en')
+const language = signal(localStorage.getItem("language") ? localStorage.getItem("language") : window.location.hostname == 'xardam-dev.vercel.app' ? 'fr' : 'en')
 const noOs = signal(false);
 const batterySignal = signal({charging: false, level: 0, chargingTime: 0, dischargingTime: 0});
 
