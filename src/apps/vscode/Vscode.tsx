9bb9fb617cf0b8ab042cd5a8d77dafc744eb1631
Art <arda1murtezan2@gmail.com>
2024-03-31 17:58:58 +0000

    Update Vscode.tsx

--- src/apps/vscode/Vscode.tsx
+++ src/apps/vscode/Vscode.tsx
@@ -1,7 +1,7 @@
 export default function Vscode() {
   return (
     <div class='w-full h-full bg-zinc-800'>
-      <iframe src="https://github1s.com/shadowdevfr/shadowdevfr" scrolling="no" height="100%" width="100%"></iframe>
+      <iframe src="https://github1s.com/whiteneone00/whiteneone00" scrolling="no" height="100%" width="100%"></iframe>
     </div>
   )
 }
