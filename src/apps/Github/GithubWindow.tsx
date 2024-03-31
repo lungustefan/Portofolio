c218c0755f37d6c5442a6d9a86bf3d113cb10b80
Art <arda1murtezan2@gmail.com>
2024-03-31 17:55:01 +0000

    Update GithubWindow.tsx

--- src/apps/Github/GithubWindow.tsx
+++ src/apps/Github/GithubWindow.tsx
@@ -5,7 +5,7 @@ export default function GithubWindow() {
     <div class='bg-zinc-700 flex flex-col items-center justify-center h-full w-full rounded-b-md'>
         <img src='/apps/github.png' class='w-[100px] mb-4' alt='Github logo' />
         <p class='text-white text-2xl mb-4'>{t("app.github.content.title")}</p>
-        <a href='https://github.com/shadowdevfr' class='text-gray-400 hover:text-white text-2xl mb-4 hover:scale-105 transition' target='_blank'>github.com/shadowdevfr</a>
+        <a href='https://github.com/whiteneone00' class='text-gray-400 hover:text-white text-2xl mb-4 hover:scale-105 transition' target='_blank'>github.com/whiteneone00</a>
     </div>
   )
 }
