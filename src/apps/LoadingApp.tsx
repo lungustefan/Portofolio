94108914e952f4f83d8335b1199ee6887955b597
Jadify-eu <jadifyeu@gmail.com>
2024-05-06 13:36:26 +0000

    Update 89 files
    - /public/3rdparty/cloudflare.png
    - /public/3rdparty/icons8.png
    - /public/3rdparty/preact.png
    - /public/3rdparty/tailwind.svg
    - /public/3rdparty/vitejs.svg
    - /public/apps/github.png
    - /public/apps/pepsifolder.png
    - /public/apps/portfolio.png
    - /public/apps/skills.png
    - /public/apps/terminal.png
    - /public/apps/vscode.svg
    - /public/apps/welcome.png
    - /public/context/about.png
    - /public/context/customize.png
    - /public/flags/fr.svg
    - /public/flags/gb.svg
    - /public/icons/hide.svg
    - /public/icons/maximise.svg
    - /public/icons/minimise.svg
    - /public/icons/x.svg
    - /public/wallpapers/1.webp
    - /public/wallpapers/2.webp
    - /public/wallpapers/3.webp
    - /public/wallpapers/4.webp
    - /public/wallpapers/5.webp
    - /public/wallpapers/6.webp
    - /public/wallpapers/7.webp
    - /public/wallpapers/8.webp
    - /public/cat.svg
    - /public/logo.jpeg
    - /public/logo.jpg
    - /public/logo.png
    - /public/logo.svg
    - /src/i18n/en.json
    - /src/i18n/fr.json
    - /src/i18n/i18n.ts
    - /src/lib/AppWindowHandler.tsx
    - /src/lib/AppsWindowsManager.tsx
    - /src/lib/DesktopAppIcon.tsx
    - /src/lib/DesktopContextMenu.tsx
    - /src/lib/NoOperatingSystem.tsx
    - /src/lib/PhoneStatusBar.tsx
    - /src/lib/Taskbar.tsx
    - /src/lib/TaskbarLanguagePopup.tsx
    - /src/lib/TaskbarRunningTaskElement.tsx
    - /src/lib/TaskbarSettingsPopup.tsx
    - /src/lib/TaskbarStartMenu.tsx
    - /src/lib/WelcomePopup.tsx
    - /src/lib/Window.tsx
    - /src/lib/WindowFullScreen.tsx
    - /src/style/animate.min.css
    - /src/style/biosfont.css
    - /src/style/font.css
    - /src/style/index.css
    - /src/apps/AboutMe/AboutMe.tsx
    - /src/apps/AboutWebsite/About.tsx
    - /src/apps/Customize/Customize.tsx
    - /src/apps/Debug/DebuggerWindow.tsx
    - /src/apps/Github/GithubWindow.tsx
    - /src/apps/PepsiTheCat/Pepsi.tsx
    - /src/apps/Terminal/Terminal.tsx
    - /src/apps/Terminal/commands.tsx
    - /src/apps/vscode/Vscode.tsx
    - /src/apps/MyProjects/lib/ProjectCard.tsx
    - /src/apps/Skills/lib/SkillCard.tsx
    - /src/apps/Skills/lib/SkillTab.tsx
    - /src/apps/MyProjects/MyProjects.tsx
    - /src/apps/MyProjects/ProjectInfo.tsx
    - /src/apps/Skills/SkillsMain.tsx
    - /src/apps/Apps.tsx
    - /src/apps/LoadingApp.tsx
    - /src/BatteryHelper.tsx
    - /src/BootScreen.tsx
    - /src/Desktop.tsx
    - /src/Signals.ts
    - /src/app.tsx
    - /src/data.ts
    - /src/main.tsx
    - /src/vite-env.d.ts
    - /types/.d.ts
    - /.gitignore
    - /tsconfig.json
    - /tsconfig.node.json
    - /vite.config.ts
    - /tailwind.config.js
    - /postcss.config.js
    - /package.json
    - /package-lock.json
    - /index.html

--- src/apps/LoadingApp.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingApp() {
-  return (
-    <div class='h-full w-full bg-zinc-800'>
-        
-    </div>
-  )
-}
--- src/apps/vscode/Vscode.tsx
+++
@@ -1,7 +0,0 @@
-export default function Vscode() {
-  return (
-    <div class='w-full h-full bg-zinc-800'>
-      <iframe src="https://github1s.com/whiteneone00/whiteneone00" scrolling="no" height="100%" width="100%"></iframe>
-    </div>
-  )
-}
