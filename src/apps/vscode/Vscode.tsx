63f7896c3b19056467fd60b7c36e5c03333d544f
Lungu Stefan-Gabriel <hi@lungustefan.ro>
2024-11-23 09:29:31 +0200

    Update Vscode.tsx

--- src/apps/vscode/Vscode.tsx
+++ src/apps/vscode/Vscode.tsx
@@ -1,7 +1,7 @@
 export default function Vscode() {
   return (
     <div class='w-full h-full bg-zinc-800'>
-      <iframe src="https://github.com/lungustefan/lungustefan" scrolling="no" height="100%" width="100%"></iframe>
+      <iframe src="https://github1s.com/lungustefan/Portofolio" scrolling="no" height="100%" width="100%"></iframe>
     </div>
   )
 }
